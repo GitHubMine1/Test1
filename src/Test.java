da79a167b8b4633b66baa88a8c310ff95b7d948a
Lu <backpack_mine@163.com>
2015-04-20 18:02:50 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+/**
+ * Created by luqun on 15/4/20.
+ */
+
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 我的主类
+ */
+public class Test {
+    static String name;
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+
+        name = "张三";
+        System.out.println(name);
+    }
+
+    public Test()
+
+    {
+        super();
+        int a = 10;
+
+
+    }
+}
